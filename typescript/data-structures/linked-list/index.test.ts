b10d1bc5c83026b713dbbc83f363687faffba251
tatimblin@gmail.com <tatimblin@gmail.com>
2022-02-12 11:55:34 -0500

    add or remove from specific index

--- typescript/data-structures/linked-list/index.test.ts
+++ typescript/data-structures/linked-list/index.test.ts
@@ -1,5 +1,7 @@
 import { LinkedList, LinkedListNode } from './index';
 
+const sampleText = 'A little learning is a dangerous thing';
+
 test('LinkedList: add head node', () => {
   const LL = new LinkedList();
   LL.add('test');
@@ -14,4 +16,22 @@ test('LinkedList: add multiple nodes', () => {
   LL.add('test');
 
   expect(LL.size).toEqual(2);
+});
+
+test('LinkedList: add node at specific index', () => {
+  const LL = new LinkedList();
+
+  sampleText.split(' ').forEach(word => LL.add(word));
+  LL.insertAt('really', 4);
+
+  expect(LL.size).toEqual(8);
+});
+
+test('LinkedList: remove node at specific index', () => {
+  const LL = new LinkedList();
+
+  sampleText.split(' ').forEach(word => LL.add(word));
+  LL.removeFrom(2);
+
+  expect(LL.size).toEqual(6);
 });
--- typescript/data-structures/linked-list/index.ts
+++ typescript/data-structures/linked-list/index.ts
@@ -1,6 +1,6 @@
 class Node {
   element: any;
-  next: string | null;
+  next: Node | null;
 
   constructor(element) {
     this.element = element;
@@ -19,11 +19,10 @@ class LinkedList {
 
   add(element) {
     const node = new Node(element);
-    let current;
 
     if (this.head === null) this.head = node;
     else {
-      current = this.head;
+      let current = this.head;
 
       // go to last node
       while (current.next) {
@@ -34,6 +33,58 @@ class LinkedList {
     }
 
     this.size += 1;
+  }
+
+  insertAt(element: any, index: number) {
+    if (index < 0 || index > this.size) return console.warn('Index out of bounds');
+    else {
+      const node = new Node(element);
+      
+      if (index === 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        let current = this.head;
+        let previous = null;
+        let iteration = 0;
+
+        // Stop at the index
+        while (iteration < index) {
+          previous = current;
+          current = current.next;
+          iteration += 1;
+        }
+
+        // Add what's left to the new node and
+        // then replace it with the new node
+        node.next = current;
+        previous.next = node;
+      }
+
+      this.size += 1;
+    }
+  }
+
+  removeFrom(index: number) {
+    if (index < 0 || index > this.size) return console.warn('Index out of bounds');
+    else {
+      if (index === 0) this.head = this.head.next;
+      else {
+        let current = this.head;
+        let previous = null;
+        let iteration = 0;
+
+        while (iteration < index) {
+          previous = current;
+          current = current.next;
+          iteration += 1;
+        }
+
+        previous.next = current.next;
+      }
+
+      this.size -= 1;
+    }
   }
 }
 
